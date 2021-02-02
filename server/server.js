dcff63b98c7827a82a88b8bae18786a5c6296699
Stas Lifanov <holidayonmail@gmail.com>
2021-02-03 01:54:03 +0300

    Project created

--- server/server.js
+++ server/server.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//=============MODELS========================
+const { User } = require("./models/user");
+//===========================================
+//              USERS
+//===========================================
+
+app.post("/api/users/register", (req, res) => {
+  res.status(200);
+});
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
